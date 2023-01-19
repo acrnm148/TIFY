cd055c93766b52d3854ed943ed1b8a951be4f5aa
wallby150 <duddlf1025@gmail.com>
2023-01-20 00:01:37 +0900

    Login try

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -2,6 +2,7 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
+import Login from "./Login";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -38,6 +39,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <Login />
     </div>
   );
 }
---
+++ front/src/Login.tsx
@@ -0,0 +1,33 @@
+import "./Login.css";
+
+function Login() {
+  return (
+    <div className="grayBackground">
+      <div className="insideBox">
+        <p className="title">LOGIN to TIFY</p>
+        <div className="loginBox">
+          <div className="emailBox">
+            <p>이메일</p>
+            <form className="emailForm">
+              <input type="text" className="inputBox" id="emailForm" />
+            </form>
+            <p>비밀번호</p>
+            <form>
+              <input type="text" className="inputBox" />
+            </form>
+            <label>
+              <input type="checkbox" name="color" value="blue" /> 로그인 정보
+              저장
+            </label>
+            <button type="submit" className="loginButton">
+              Login
+            </button>
+            <button className="findPassword">Forgot password?</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
