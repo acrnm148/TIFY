4cf99aa0405292de77d19713da7be0eb107dac78
강수나 <acrnm148@gmail.com>
2023-01-20 15:37:28 +0900

    [ksn] fix

--- gifthub_ing/src/main/java/com/javatechie/crud/example/repository/CartRepository.java
+++
@@ -1,8 +0,0 @@
-package com.javatechie.crud.example.repository;
-
-import com.javatechie.crud.example.entity.Cart;
-import com.javatechie.crud.example.entity.Img;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CartRepository extends JpaRepository<Cart, Integer> {
-}
--- gifthub_ing/src/main/java/com/javatechie/crud/example/repository/ImgRepository.java
+++
@@ -1,7 +0,0 @@
-package com.javatechie.crud.example.repository;
-
-import com.javatechie.crud.example.entity.Img;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ImgRepository extends JpaRepository<Img, Integer> {
-}
