cb70bb81f4d7b59a8a14e19d3867adbe9fe81eec
강수나 <acrnm148@gmail.com>
2023-01-20 17:06:17 +0900

    Deleted backend/.gradle/7.6/checksums/checksums.lock, backend/.gradle/7.6/checksums/md5-checksums.bin, backend/.gradle/7.6/checksums/sha1-checksums.bin, backend/.gradle/7.6/dependencies-accessors/dependencies-accessors.lock, backend/.gradle/7.6/dependencies-accessors/gc.properties, backend/.gradle/7.6/executionHistory/executionHistory.lock, backend/.gradle/7.6/fileChanges/last-build.bin, backend/.gradle/7.6/fileHashes/fileHashes.lock, backend/.gradle/7.6/gc.properties, backend/.gradle/buildOutputCleanup/buildOutputCleanup.lock, backend/.gradle/buildOutputCleanup/cache.properties, backend/.gradle/vcs-1/gc.properties, backend/.idea/.gitignore, backend/.idea/compiler.xml, backend/.idea/gradle.xml, backend/.idea/jarRepositories.xml, backend/.idea/misc.xml, backend/.idea/vcs.xml, backend/src/main/resources/application.yaml, backend/HELP.md, backend/build.gradle, gifthub_ing/gradle/wrapper/gradle-wrapper.jar, gifthub_ing/gradle/wrapper/gradle-wrapper.properties, gifthub_ing/src/main/java/com/javatechie/crud/example/common/CartDTO.java, gifthub_ing/src/main/java/com/javatechie/crud/example/common/CommonMethods.java, gifthub_ing/src/main/java/com/javatechie/crud/example/controller/CartController.java, gifthub_ing/src/main/java/com/javatechie/crud/example/controller/ProductController.java, gifthub_ing/src/main/java/com/javatechie/crud/example/controller/WishController.java, gifthub_ing/src/main/java/com/javatechie/crud/example/entity/Cart.java, gifthub_ing/src/main/java/com/javatechie/crud/example/entity/CartItem.java, gifthub_ing/src/main/java/com/javatechie/crud/example/entity/Gift.java, gifthub_ing/src/main/java/com/javatechie/crud/example/entity/Img.java, gifthub_ing/src/main/java/com/javatechie/crud/example/entity/Option.java, gifthub_ing/src/main/java/com/javatechie/crud/example/entity/Product.java, gifthub_ing/src/main/java/com/javatechie/crud/example/entity/Wish.java, gifthub_ing/src/main/java/com/javatechie/crud/example/repository/CartItemRepository.java, gifthub_ing/src/main/java/com/javatechie/crud/example/repository/CartRepository.java, gifthub_ing/src/main/java/com/javatechie/crud/example/repository/GiftRepository.java, gifthub_ing/src/main/java/com/javatechie/crud/example/repository/ImgRepository.java, gifthub_ing/src/main/java/com/javatechie/crud/example/repository/ProductRepository.java, gifthub_ing/src/main/java/com/javatechie/crud/example/repository/WishRepository.java, gifthub_ing/src/main/java/com/javatechie/crud/example/service/CartItemService.java, gifthub_ing/src/main/java/com/javatechie/crud/example/service/CartService.java, gifthub_ing/src/main/java/com/javatechie/crud/example/service/GiftService.java, gifthub_ing/src/main/java/com/javatechie/crud/example/service/ImgService.java, gifthub_ing/src/main/java/com/javatechie/crud/example/service/ProductService.java, gifthub_ing/src/main/java/com/javatechie/crud/example/service/WishService.java, gifthub_ing/src/main/java/com/javatechie/crud/example/SpringBootCrudExample2Application.java, gifthub_ing/src/main/resources/application.properties, gifthub_ing/src/test/java/com/javatechie/crud/example/SpringBootCrudExample2ApplicationTests.java, gifthub_ing/src/test.zip, gifthub_ing/.gitignore, gifthub_ing/build.gradle, gifthub_ing/gradlew, gifthub_ing/gradlew.bat, gifthub_ing/settings.gradle, TIFY.postman_collection.json, readme.md

--- gifthub_ing/src/main/java/com/javatechie/crud/example/repository/CartItemRepository.java
+++
@@ -1,8 +0,0 @@
-package com.javatechie.crud.example.repository;
-
-import com.javatechie.crud.example.entity.Cart;
-import com.javatechie.crud.example.entity.CartItem;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CartItemRepository extends JpaRepository<CartItem, Integer> {
-}
--- gifthub_ing/src/main/java/com/javatechie/crud/example/repository/WishRepository.java
+++
@@ -1,7 +0,0 @@
-package com.javatechie.crud.example.repository;
-
-import com.javatechie.crud.example.entity.Wish;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface WishRepository extends JpaRepository<Wish, Integer> {
-}
