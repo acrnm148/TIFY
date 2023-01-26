2e6d7879a2aaa5c541c7519ca0f03c4cd7f77a58
이상훈_부울경1반 <a0511a@naver.com>
2023-01-26 14:49:20 +0900

    lsh S08P12E208-148

--- 건들지마시오 병합예정-lsh/src/main/java/com/tify/back/exception/QNANotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.tify.back.exception;
-
-public class QNANotFoundException extends RuntimeException {
-    public QNANotFoundException(String message) {
-        super(message);
-    }
-}
