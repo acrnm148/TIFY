d5319f09fcdc3476c6e042d837004bbbc060168f
beetrootfarmer <pos04118@gmail.com>
2023-01-30 17:46:29 +0900

    S08P12E208-229 [기프트허브 상세페이지 레이아웃]

--- front/src/pages/GiftItemDetailPage.tsx
+++ front/src/pages/GiftItemDetailPage.tsx
@@ -1,10 +1,29 @@
 import { useParams } from "react-router";
+import "../css/GiftItemDetailPage.styles.css";
 
 export default function GiftItemDetailPage(){
     let {giftId} = useParams();
     return(
-        <>
+        <div className="gift-item-detail-container">
             <h1> {giftId}번 상품의 디테일페이지</h1>
-        </>
+            <p>뒤로가기</p>
+            <div className="product-info">
+                <img src="" alt="" />
+                <div className="product-info-right">
+                    <div className="product-name-like">
+                        <p></p>
+                        <img src="" alt="" />
+                    </div>
+                    <div className="product-price-option">
+                        <p></p>
+                        <div></div>
+                    </div>
+                    <div className="">
+
+                    </div>
+                </div>
+            </div>
+            <div className="product-image"></div>
+        </div>
     )
 }
