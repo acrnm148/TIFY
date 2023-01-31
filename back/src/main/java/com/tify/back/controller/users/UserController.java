aa131e9df0ea31cdb69b52968338d7b4d8f38855
a0511a <a0511a@naver.com>
2023-02-01 05:58:55 +0900

    lsh 깃오류수정

--- back/src/main/java/com/tify/back/controller/users/UserController.java
+++ back/src/main/java/com/tify/back/controller/users/UserController.java
@@ -1,33 +1,28 @@
 package com.tify.back.controller.users;
-import com.tify.back.dto.users.UserDTO;
-import com.tify.back.dto.admin.createUserDto;
-import com.tify.back.model.users.User;
+import com.tify.back.dto.users.SearchedUserDto;
 import com.tify.back.service.users.UserService;
-
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.*;
 
+import static java.lang.Long.parseLong;
+
 @RestController
+@RequestMapping("/api")
 public class UserController {
+
 	private final UserService userService;
+
 	public UserController(UserService userService) {
 		this.userService = userService;
 	}
+
 	@GetMapping("/searchuser/{nickname}")
-	public List<UserDTO> searchUserByNickname(@PathVariable String nickname) {
-		List<User> users = userService.searchUserByNickname(nickname);
-		List<UserDTO> userDTOs = new ArrayList<>();
-		for (User user : users) {
-			UserDTO userDTO = new UserDTO();
-			userDTO.setId(user.getId());
-			userDTO.setProfileImg(user.getProfileImg());
-			userDTO.setNickname(user.getNickname());
-			userDTO.setEmail(user.getEmail());
-			userDTOs.add(userDTO);
-		}
-		return userDTOs;
+	public List<SearchedUserDto> searchUserByNickname(@PathVariable String nickname, @RequestHeader("Authorization") String token) {
+		String myId = userService.getUserid(token);
+		List<SearchedUserDto> users = userService.searchUserByNickname(nickname,parseLong(myId));
+
+		return users;
 	}
 
 }
