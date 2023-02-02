a8fbffabd9af42a94b2ea9952beaad1b4e746c27
부울경_1반_강기한 <rkdrlgks321@naver.com>
2023-02-03 03:33:16 +0900

    Update back/src/main/java/com/tify/back/Firebase/fcmController.java

--- back/src/main/java/com/tify/back/Firebase/fcmController.java
+++ back/src/main/java/com/tify/back/Firebase/fcmController.java
@@ -21,19 +21,28 @@ public class fcmController {
     private final FirebaseConfig firebase;
 
     @PostMapping
-    public String fcmTest() throws FirebaseAuthException {
-        String uid = "rkdrlgks321-naver-com";
+    public String fcmTest(@RequestBody String email) throws FirebaseAuthException {
+
+        String uid = email.replace("@","-").replace(".","-");
+        System.out.println(uid);
+//        String uid = "rkdrlgks321-naver-com";
         String customToken = FirebaseAuth.getInstance().createCustomToken(uid);
         return customToken;
-
     }
 
     @GetMapping("/addData")
-    public void addData() {
+    public Map<String, Object> addData() {
         FirebaseDatabase database = FirebaseDatabase.getInstance("https://tify-noti-default-rtdb.firebaseio.com/");
         DatabaseReference reference = database.getReference("test/tify/rkdrlgks321-naver-com");
         Map<String, Object> updates = new HashMap<>();
         updates.put("data", "Hello, World!");
+        updates.put("time", System.currentTimeMillis());
+        updates.put("state", false);
+        updates.put("title", "test title");
+        updates.put("content", "content");
+        updates.put("image_url","img");
+        updates.put("link_url","link");
+        updates.put("type","friends");
         reference.push().setValue(updates,new DatabaseReference.CompletionListener() {
             @Override
             public void onComplete(DatabaseError error, DatabaseReference ref) {
@@ -44,15 +53,5 @@ public class fcmController {
                 }
             }
         });
-//        reference.updateChildren(updates, new DatabaseReference.CompletionListener() {
-//            @Override
-//            public void onComplete(DatabaseError error, DatabaseReference ref) {
-//                if (error == null) {
-//                    System.out.println("Data updated successfully");
-//                } else {
-//                    System.out.println("Data update failed: " + error.getMessage());
-//                }
-//            }
-//        });
-    }
+        return updates;
 }
