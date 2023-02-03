ef666e42b6052d2ccdf637384586d5839e1ab79e
wallaby150 <duddlf1025@gmail.com>
2023-02-03 11:43:21 +0900

    refreshtoken 수정 중

--- front/src/modules/Auth/CheckToken.tsx
+++ front/src/modules/Auth/CheckToken.tsx
@@ -19,6 +19,11 @@ export function CheckTokenByKey(key: string) {
   const refreshToken = getCookieToken();
   const dispatch = useDispatch();
   // const navigate = useNavigate();
+  console.log(
+    authenticated,
+    expireTime,
+    'authenticated, expireTime 이렇습니다!!!',
+  );
 
   useEffect(() => {
     const checkAuthToken = async () => {
@@ -65,62 +70,65 @@ export function CheckTokenByKey(key: string) {
   };
 }
 
-export function CheckToken() {
-  const [isAuth, setIsAuth] = useState('Loaded');
-  const { authenticated, expireTime } = useSelector(
-    (state: RootState) => state.authToken,
-  );
-  const refreshToken = getCookieToken();
+// export function CheckToken() {
+//   const [isAuth, setIsAuth] = useState('Loaded');
+//   const { authenticated, expireTime } = useSelector(
+//     (state: RootState) => state.authToken,
+//   );
+//   const refreshToken = getCookieToken();
+//   console.log(refreshToken, '이게 리프레쉬 토큰!!!!!!');
+//   const dispatch = useDispatch();
+//   // const navigate = useNavigate();
+
+//   useEffect(() => {
+//     const checkAuthToken = () => {
+//       // 리프레쉬 토큰이 없다면
+//       if (refreshToken === undefined) {
+//         // access 토큰 없애고, 인증 여부를 Failed로 한다.
+//         dispatch(DELETE_TOKEN());
+//         setIsAuth('Failed');
+//         console.log('리프레쉬토큰 없는 녀석');
+//         // return navigate('/');
+//         // 리프레쉬 토큰 있으면
+//       } else {
+//         // 인증 여부 및 기간 만료 확인
+//         if (authenticated && new Date().getTime() < expireTime) {
+//           console.log('아직 쓸만한 access token');
+//           setIsAuth('Success');
+//         } else {
+//           // access코드 이상하면 리프레쉬 토큰으로 리프레쉬 시도
+//           const response = await requestToken(refreshToken);
+
+//           if (response) {
+//             // const token = response.json.access_token;
+//             // dispatch(SET_USERID(token));
+//             // dispatch(SET_TOKEN(token));
+//             console.log('리프레쉬 성공');
+//             setIsAuth('Success');
+//           } else {
+//             dispatch(DELETE_TOKEN());
+//             removeCookieToken();
+//             setIsAuth('Failed');
+//             console.log('리프레쉬 실패');
+//             alert('로그아웃 되셨습니다.');
+//             // return navigate('/');
+//           }
+//         }
+//       }
+//     };
+//     // checkAuthToken();
+//   }, [dispatch]);
+
+//   return {
+//     isAuth,
+//   };
+// }
+
+async function requestToken(refreshToken: string) {
+  const { userId } = useSelector((state: RootState) => state.authToken.userId);
   const dispatch = useDispatch();
-  // const navigate = useNavigate();
 
-  useEffect(() => {
-    const checkAuthToken = () => {
-      // 리프레쉬 토큰이 없다면
-      if (refreshToken === undefined) {
-        // access 토큰 없애고, 인증 여부를 Failed로 한다.
-        dispatch(DELETE_TOKEN());
-        setIsAuth('Failed');
-        console.log('리프레쉬토큰 없는 녀석');
-        // return navigate('/');
-        // 리프레쉬 토큰 있으면
-      } else {
-        // 인증 여부 및 기간 만료 확인
-        if (authenticated && new Date().getTime() < expireTime) {
-          console.log('아직 쓸만한 access token');
-          setIsAuth('Success');
-        } else {
-          // access코드 이상하면 리프레쉬 토큰으로 리프레쉬 시도
-          const response = requestToken(refreshToken);
-
-          if (response) {
-            // const token = response.json.access_token;
-            // dispatch(SET_USERID(token));
-            // dispatch(SET_TOKEN(token));
-            console.log('리프레쉬 성공');
-            setIsAuth('Success');
-          } else {
-            dispatch(DELETE_TOKEN());
-            removeCookieToken();
-            setIsAuth('Failed');
-            console.log('리프레쉬 실패');
-            alert('로그아웃 되셨습니다.');
-            // return navigate('/');
-          }
-        }
-      }
-    };
-    checkAuthToken();
-  }, [dispatch]);
-
-  return {
-    isAuth,
-  };
-}
-
-export const requestToken = (refreshToken: string) => {
-  const { userId } = useSelector((state: RootState) => state.authToken);
-  const dispatch = useDispatch();
+  console.log('리프레쉬 요청 보내보기');
 
   try {
     axios
@@ -131,6 +139,8 @@ export const requestToken = (refreshToken: string) => {
         { headers: { refreshToken } },
       )
       .then((res) => {
+        console.log('리프레쉬 요청한 거 받음');
+        console.log(res);
         // console.log(res);
         // console.log(res.data.accessToken);
         // const tokens = {
@@ -143,6 +153,7 @@ export const requestToken = (refreshToken: string) => {
 
         // 재발급 성공
         // 리프레쉬 토큰 재설정하기
+
         return true;
       });
   } catch (err) {
@@ -177,4 +188,4 @@ export const requestToken = (refreshToken: string) => {
   //   } else {
   //     return statusError;
   //   }
-};
+}
