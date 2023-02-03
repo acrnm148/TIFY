0fa2818208bcd2a2129d37c3cb3dd79615be7493
이상훈_부울경1반 <a0511a@naver.com>
2023-02-03 14:17:34 +0900

    Update back/src/main/java/com/tify/back/controller/users/UserController.java

--- back/src/main/java/com/tify/back/controller/users/UserController.java
+++ back/src/main/java/com/tify/back/controller/users/UserController.java
@@ -10,6 +10,7 @@ import java.util.List;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/api")
 public class UserController {
 	private final UserService userService;
 	public UserController(UserService userService) {
