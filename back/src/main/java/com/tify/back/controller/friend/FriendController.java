6a432e79d3b2ca9484523acfc9aa973f3688174d
이상훈_부울경1반 <a0511a@naver.com>
2023-02-03 17:18:43 +0900

    Update back/src/main/java/com/tify/back/controller/friend/FriendController.java

--- back/src/main/java/com/tify/back/controller/friend/FriendController.java
+++ back/src/main/java/com/tify/back/controller/friend/FriendController.java
@@ -7,11 +7,7 @@ import com.tify.back.model.friend.FriendStatus;
 import com.tify.back.service.friend.FriendService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
