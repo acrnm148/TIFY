5ae854356ba99137e50646137e5ca67af581bf4c
이상훈_부울경1반 <a0511a@naver.com>
2023-02-06 17:34:49 +0900

    유저찾기 페이지 중간저장

--- front/src/pages/FriendsPage.tsx
+++ front/src/pages/FriendsPage.tsx
@@ -1,38 +1,92 @@
-import React, {  useEffect, useRef, useState,  } from "react";
+import React, { useState, useEffect } from "react";
+import { useSelector } from 'react-redux';
+import { RootState } from '../store/Auth';
+import axios from "axios";
+import "../css/friendspage.styles.css"
 
-const FriendsPage=() =>{
-    const [imgBase64, setImgBase64] = useState(""); // 파일 base64
-  const [imgFile, setImgFile] = useState(null);	//파일	
-  
-  const handleChangeFile = (event:any) => {
-    let reader = new FileReader();
-
-    reader.onloadend = () => {
-      // 2. 읽기가 완료되면 아래코드가 실행됩니다.
-      const base64 = reader.result;
-      if (base64) {
-        setImgBase64(base64.toString()); // 파일 base64 상태 업데이트
-      }
-    }
-    if (event.target.files[0]) {
-      reader.readAsDataURL(event.target.files[0]); // 1. 파일을 읽어 버퍼에 저장합니다.
-      setImgFile(event.target.files[0]); // 파일 상태 업데이트
-      console.log(imgFile, 'imgFile') // 
-    }
-  }
-  
-  return (
-    <div className="App">
-      <div style={{"backgroundColor": "#efefef", "width":"150px", "height" : "150px", "overflow":"hidden"}}>
-        <img src={imgBase64} alt="" />
-      </div>
-      <div>
-      	{/* onChange 추가 */}
-        <input type="file" name="imgFile" id="imgFile" onChange={handleChangeFile}/>
-      </div>
-    </div>
-  );
+interface User {
+  id: number;
+  name: string;
+  profileImg: string;
+  nickname: string;
+  email: string;
+  state: string;
 }
 
-export default FriendsPage;
+const FriendsPage: React.FC = () => {
+  const [nickname, setNickname] = useState("");
+  const [users, setUsers] = useState<Array<User>>([]);
+  const accessToken = useSelector(
+    (state: RootState) => state.authToken.accessToken,
+  );
 
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    try {
+      const response = await axios.get(`https://i8e208.p.ssafy.io/api/searchuser/${nickname}`, {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+          'Content-type': 'application/json',
+        }
+      });
+      const friends = response.data;
+      
+      setUsers([...friends]);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+  <div className="backcolor">
+    <div className="items-center">
+      <UserSearchBar
+        nickname={nickname}
+        onNicknameChange={event => setNickname(event.target.value)}
+        onSubmit={handleSubmit}
+      />
+<div className="flex flex-wrap mx-12">
+  {users.map(user => (
+    <div className="w-1/5 p-6 mx-6 my-2 cardcolor items-center" key={user.id}>
+      <img className="img-4" src={user.profileImg}/>
+        <p className="w-96 text-lg text-center">{user.name}</p>
+        <p className="w-96 text-xs text-center">{user.nickname}</p>
+        <p className="w-96 text-xs text-center">
+        {user.state === 'ACCEPTED' ? '친구' : 
+         user.state === 'RECEIVED' ? '요청받음' :
+         user.state === 'REQUESTED' ? '요청보냄' : 
+         '친구요청'}</p>
+        
+      </div>
+  ))}
+</div>
+    </div>
+    </div>
+  );
+};
+
+interface UserSearchBarProps {
+  nickname: string;
+  onNicknameChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  onSubmit: (event: React.FormEvent<HTMLFormElement>) => void;
+}
+
+const UserSearchBar: React.FC<UserSearchBarProps> = ({
+  nickname,
+  onNicknameChange,
+  onSubmit
+}) => (
+  <form onSubmit={onSubmit}>
+    <input
+      className="searchbar"
+      type="text"
+      placeholder="Enter nickname"
+      value={nickname}
+      onChange={onNicknameChange}
+    />
+    <button className="search-btn" type="submit"></button>
+  </form>
+);
+
+export default FriendsPage;
