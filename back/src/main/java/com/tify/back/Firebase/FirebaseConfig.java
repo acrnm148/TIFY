a4fae66aa518b591b4dbaefb9652ef3965705d92
강수나 <acrnm148@gmail.com>
2023-02-07 11:07:28 +0900

    fix : firebase

--- back/src/main/java/com/tify/back/Firebase/FirebaseConfig.java
+++ back/src/main/java/com/tify/back/Firebase/FirebaseConfig.java
@@ -23,7 +23,7 @@ public class FirebaseConfig {
     public void init(){
         try{
             String userDirectory = System.getProperty("user.dir");
-            String filePath = userDirectory + "/src/main/resources/firebaseconfig/tify-noti-firebase.json";
+            String filePath = userDirectory + "src/main/resources/firebaseconfig/tify-noti-firebase.json";
 
             FileInputStream serviceAccount =
                     new FileInputStream(filePath);
