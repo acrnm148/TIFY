b1fa28a27e97ef8c6b86d70eef489592e1d38144
부울경_1반_강기한 <rkdrlgks321@naver.com>
2023-02-07 11:24:52 +0900

    [kgh] fix-firebase-path-error

--- back/src/main/java/com/tify/back/Firebase/FirebaseConfig.java
+++ back/src/main/java/com/tify/back/Firebase/FirebaseConfig.java
@@ -3,30 +3,20 @@ package com.tify.back.Firebase;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 
 import javax.annotation.PostConstruct;
-import java.io.FileInputStream;
+import java.io.InputStream;
 
 @Configuration
 public class FirebaseConfig {
-
-    @Value("${firebase.credential.resource-path}")
-    private String keyPath;
-
-    @Bean
+    InputStream serviceAccount = this.getClass().getResourceAsStream("/firebaseconfig/tify-noti-firebase.json");
     @Primary
     @PostConstruct
     public void init(){
         try{
             String userDirectory = System.getProperty("user.dir");
-            String filePath = userDirectory + "/src/main/resources/firebaseconfig/tify-noti-firebase.json";
-
-            FileInputStream serviceAccount =
-                    new FileInputStream(filePath);
             FirebaseOptions options = FirebaseOptions.builder()
                     .setCredentials(GoogleCredentials.fromStream(serviceAccount))
                     .setServiceAccountId("firebase-adminsdk-35g8k@tify-noti.iam.gserviceaccount.com")
