84276e07a5409457355980b965dc0c90c0d00d00
octover1025 <octover1025@naver.com>
2023-02-09 15:36:55 +0900

    일촌위시

--- front/src/pages/mypage/MyFriend.tsx
+++ front/src/pages/mypage/MyFriend.tsx
@@ -1,5 +1,5 @@
 // import type { WishProps } from '../interface/interface';
-import { useState } from 'react';
+import { ClassAttributes, HTMLAttributes, useState, useEffect } from 'react';
 import '../../css/mypage/myFriend.styles.css';
 import profileIcon from '../../assets/iconDefaultProfile.png';
 import Slider from 'react-slick';
@@ -10,63 +10,78 @@ import { useSelector } from 'react-redux';
 import { RootState } from '../../store/Auth';
 import axios from 'axios';
 
+// slider
+import ReactSlider from 'react-slider';
+import 'rc-slider/assets/index.css';
+
 export function Friend() {
   const userId = useSelector((state: RootState) => state.authToken.userId);
-  const [friendWish, setFriendWish] = useState<Array<string>>();
+  const [friendWishList, setFriendWishList] = useState<Array<any>>();
 
-  const API_URL = `https://i8e208.p.ssafy.io/api/wishfriend/${userId}`;
-  // const API_URL = `https://i8e208.p.ssafy.io/api/friendsinfo/119`;
-  const result = [];
-  axios
-    .get(API_URL)
-    .then((res) => {
-      // console.log(res.data, '내가 바로 친구놈들의 wish 입니다!!!!');
-      // console.log(typeof res.data, '내가 바로 친구놈들의 wish type 입니다!!!!');
-      if (res.data.length > 0) {
-        const wishFriendObj = Object.values(res.data);
-        // console.log(wishFriendObj, 'wishFriendObj입니다.');
-        for (let key in wishFriendObj) {
-          const value = wishFriendObj[key];
-          console.log(value, 'value는 이거야.');
-          console.log(
-            Object.values(value),
-            'wishFriendObj 순회 돌린 결과의 밸류',
-          );
-          const friendWishes = Object.values(value);
-          console.log(typeof friendWishes);
-          for (let key in value) {
-            const friendWishes = value[key];
-            for (let key in friendWishes) {
-              const friendWish = friendWishes[key];
-              console.log(friendWish);
-              if (friendWish.finishYN !== 'Y') {
-                const temp: { any } = {};
-                console.log(friendWish, '안 끝난 frinedWish');
-                temp.wishId = friendWish.id;
-                temp.totPrice = friendWish.totPrice;
+  useEffect(() => {
+    const API_URL = `https://i8e208.p.ssafy.io/api/wishfriend/${userId}`;
+    // const API_URL = `https://i8e208.p.ssafy.io/api/friendsinfo/119`;
+    const result: any[] = [];
+    axios
+      .get(API_URL)
+      .then((res) => {
+        console.log('일촌 위시 받아오기 성공');
+        // console.log(res.data, '내가 바로 친구놈들의 wish 입니다!!!!');
+        // console.log(typeof res.data, '내가 바로 친구놈들의 wish type 입니다!!!!');
+        if (res.data.length > 0) {
+          const wishFriendObj = Object.values(res.data);
+          // console.log(wishFriendObj, 'wishFriendObj입니다.');
+          for (let key in wishFriendObj) {
+            const value: any = wishFriendObj[key];
+            console.log(value, 'value는 이거야.');
+            console.log(
+              Object.values(value),
+              'wishFriendObj 순회 돌린 결과의 밸류',
+            );
+            const friendWishes = Object.values(value);
+            console.log(typeof friendWishes);
+            for (let key in value) {
+              const friendWishes = value[key];
+              for (let key in friendWishes) {
+                const friendWish = friendWishes[key];
+                console.log(friendWish);
+                if (friendWish.finishYN !== 'Y') {
+                  console.log(friendWish, '안 끝난 frinedWish');
+                  const data = {
+                    wishId: friendWish.id,
+                    totPrice: friendWish.totPrice,
+                    nowPrice: friendWish.nowPrice,
+                    endDate: friendWish.endDate,
+                    title: friendWish.title,
+                    percent: (friendWish.nowPrice / friendWish.totPrice) * 100,
+                    userName: friendWish.user.username,
+                    nickName: friendWish.user.nickname,
+                    userImg: friendWish.user.profileImg,
+                  };
+                  console.log(data, 'data는 요것!!!!!!!!!!!!!!!!!');
+                  result.push(data);
+                }
               }
             }
           }
         }
-      }
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-
-  let [wishes, setWishes] = useState([
-    {
-      title: '왈라비 주머니의 고영일 생일 위시',
-      name: '기한',
-      nickname: '기한짱짱맨',
-    },
-    {
-      title: '왈라비 주머니의 고영일 생일 위시',
-      name: '기한',
-      nickname: '기한짱짱맨',
-    },
-  ]);
-  console.log(wishes);
+        console.log(result, 'result는 이것입니다~~~');
+        const sortedResult: any = result.sort(function (a, b) {
+          if (a.endDate < b.endDate) {
+            return -1;
+          } else if (a.grade > b.grade) {
+            return 1;
+          } else {
+            return 0;
+          }
+        });
+        console.log(sortedResult);
+        setFriendWishList(sortedResult);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
 
   return (
     <div id="base-div">
@@ -77,41 +92,17 @@ export function Friend() {
         })}
       </div> */}
       <div className="carousel-div">
-        <Carousel></Carousel>
+        {/* <Carousel></Carousel> */}
+        <Carousel friendWishList={friendWishList}></Carousel>
       </div>
     </div>
   );
 }
 
 // 캐러셀 부분
-const Carousel = (items: any) => {
+const Carousel = ({ friendWishList }) => {
   // 옵션
-  var items: any = [
-    {
-      img: '',
-      name: '강기한1',
-      nickname: '기한짱짱맨',
-      title: 'Probably the most random thing you have ever seen!',
-    },
-    {
-      img: '',
-      name: '강기한2',
-      nickname: '기한짱짱맨',
-      title: 'Probably the most random thing you have ever seen!',
-    },
-    {
-      img: '',
-      name: '강기한3',
-      nickname: '기한짱짱맨',
-      title: 'Probably the most random thing you have ever seen!',
-    },
-    {
-      img: '',
-      name: '강기한4',
-      nickname: '기한짱짱맨',
-      title: 'Probably the most random thing you have ever seen!',
-    },
-  ];
+  console.log(friendWishList);
   var settings = {
     dots: true,
     infinite: false,
@@ -150,11 +141,14 @@ const Carousel = (items: any) => {
 
   return (
     <div className="ongoing-wishes">
-      <h2> Responsive </h2>
       <Slider {...settings}>
-        {items.map((item: any) => {
+        {friendWishList &&
+          friendWishList.map((friendWish: object) => {
+            return <WishCard friendWish={friendWish}></WishCard>;
+          })}
+        {/* {items.map((item: any) => {
           return <WishCard item={item}></WishCard>;
-        })}
+        })} */}
         <div className="ongoing-wish">
           <h3>1</h3>
         </div>
@@ -184,17 +178,36 @@ const Carousel = (items: any) => {
   );
 };
 
-function WishCard({ item }: any) {
+function WishCard({ friendWish }: any) {
+  console.log(friendWish);
   return (
     <div className="ongoing-wish">
-      <div className="profile-div">
-        <img src={profileIcon} alt="" className="profile-img" />
+      <div className="wish-profile-div">
+        <img src={friendWish.userImg} alt="" className="wish-profile-img" />
         <div className="name-div">
-          <p>{item.name}</p>
-          <p>{item.nickname}</p>
+          <p className="friend-wish-username">{friendWish.userName}</p>
+          <p className="friend-wish-nickname">{friendWish.nickName}</p>
         </div>
       </div>
-      <p>{item.title}</p>
+      <p className="friend-wish-title">"{friendWish.title}"</p>
+      <div className="wish-slider-and-label-container">
+        <div className="slider-and-label">
+          <span>진행도</span>
+          <ReactSlider
+            className="horizontal-slider-myfriend"
+            defaultValue={[friendWish.percent]}
+            disabled={true}
+            thumbClassName="example-thumb"
+            trackClassName="example-track"
+            renderTrack={(
+              props: JSX.IntrinsicAttributes &
+                ClassAttributes<HTMLDivElement> &
+                HTMLAttributes<HTMLDivElement>,
+              state: any,
+            ) => <div {...props} />} //custom track
+          />
+        </div>
+      </div>
     </div>
   );
 }
