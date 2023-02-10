cc3a77733bf99c4b1fbf8c666a727b30fdcf3299
부울경_1반_강기한 <rkdrlgks321@naver.com>
2023-02-10 17:56:35 +0900

    [kgh] S08P12E208-317

--- back/src/main/java/com/tify/back/repository/gifthub/OrderRepository.java
+++ back/src/main/java/com/tify/back/repository/gifthub/OrderRepository.java
@@ -1,7 +1,9 @@
 package com.tify.back.repository.gifthub;
 
+import org.springframework.data.domain.Page;
 import com.tify.back.model.gifthub.Order;
 import com.tify.back.model.users.User;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,4 +12,7 @@ import java.util.List;
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
     public List<Order> findAllByUser(User user);
+    Page<Order> findByUserEmail(String email, Pageable pageable);
+
+    Page<Order> findAll(Pageable page);
 }
