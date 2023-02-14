4229144b03f857741a8763da93a7f2b0e7179850
beetrootfarmer <pos04118@gmail.com>
2023-02-14 11:13:33 +0900

    빌드 충돌 해결

--- front/src/store/index.tsx
+++ front/src/store/index.tsx
@@ -1,8 +1,8 @@
-// import { configureStore } from '@reduxjs/toolkit';
-// import tokenReducer from './Auth';
+import { configureStore } from '@reduxjs/toolkit';
+import tokenReducer from './Auth';
 
-// export default configureStore({
-//   reducer: {
-//     authToken: tokenReducer,
-//   },
-// });
+export default configureStore({
+  reducer: {
+    authToken: tokenReducer,
+  },
+});
