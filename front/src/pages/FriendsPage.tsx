e0fa2073ddb40e012590075fcbe923ef173a2de9
강수나 <acrnm148@gmail.com>
2023-02-16 10:16:34 +0900

    [ksn] mod find friends

--- front/src/pages/FriendsPage.tsx
+++ front/src/pages/FriendsPage.tsx
@@ -4,11 +4,13 @@ import { RootState } from '../store/Auth';
 import axios from 'axios';
 import '../css/friendspage.styles.css';
 import '../css/giftHubPage.styles.css';
+import search_h from '../assets/miri/miri-12.png';
+import find_friends from '../assets/miri/miri-10.png';
 import GiftBoxAnimation from '../components/GiftBoxAnimation';
 import TapNameEng from '../components/TapNameEng';
 
 // alarm
-import { push, ref } from "firebase/database";
+import { push, ref } from 'firebase/database';
 import { db } from '../components/firebase';
 
 interface User {
@@ -30,45 +32,52 @@ const FriendsPage: React.FC = () => {
   const [searchPerformed, setSearchPerformed] = useState(false);
   const [totalSearched, setTotalSearched] = useState(0);
 
-
-     
   // 친구요청알림부분
   // 알림보낼 유저정보 받아오는부분
-  const [userProfile, setUserProfile] = useState<string>()
-  const [username ,setUserName] = useState<string>()
-  const pushData = (friendId:number, userId:number) => {
-		let base = "/test/tify/"; 
+  const [userProfile, setUserProfile] = useState<string>();
+  const [username, setUserName] = useState<string>();
+  const pushData = (friendId: number, userId: number) => {
+    let base = '/test/tify/';
     push(ref(db, base + friendId), {
-      text: username+'님이 친구요청을 보냈습니다.',
-      profile : userProfile,
-      interval: "Daily",
+      text: username + '님이 친구요청을 보냈습니다.',
+      profile: userProfile,
+      interval: 'Daily',
       friend: 'req',
-      friendName : username,
-      friendId : userId,
+      friendName: username,
+      friendId: userId,
     });
-    console.log('친구요청보냄')
+    console.log('친구요청보냄');
   };
-// 알림보낼 때 필요한 프로필 사진 받는 부분
- useEffect(()=>{
-    const getUser = async () =>{
-      const API_URL = `https://i8e208.p.ssafy.io/api/account/userInfo`
+  // 알림보낼 때 필요한 프로필 사진 받는 부분
+  useEffect(() => {
+    const getUser = async () => {
+      const API_URL = `https://i8e208.p.ssafy.io/api/account/userInfo`;
       axios({
         method: 'get',
         url: API_URL,
         headers: { Authorization: `Bearer ${accessToken}` },
       })
-        .then((con: { data: { profileImg: React.SetStateAction<string | undefined>; addr1: React.SetStateAction<string | undefined>; addr2: React.SetStateAction<string | undefined>; zipcode: React.SetStateAction<number | undefined>; username: React.SetStateAction<string | undefined>; }; }) => {
-          console.log('유저프로필 불러오기 성공',con.data);
-          setUserProfile(con.data.profileImg)
-          setUserName(con.data.username)
-        })
+        .then(
+          (con: {
+            data: {
+              profileImg: React.SetStateAction<string | undefined>;
+              addr1: React.SetStateAction<string | undefined>;
+              addr2: React.SetStateAction<string | undefined>;
+              zipcode: React.SetStateAction<number | undefined>;
+              username: React.SetStateAction<string | undefined>;
+            };
+          }) => {
+            console.log('유저프로필 불러오기 성공', con.data);
+            setUserProfile(con.data.profileImg);
+            setUserName(con.data.username);
+          },
+        )
         .catch((err: any) => {
           console.log('유저프로필 불러오기 실패', err);
         });
-    }
+    };
     getUser();
- },[])
-
+  }, []);
 
   const handleNextPage = () => {
     setCurrentPage(currentPage + 1);
@@ -127,10 +136,10 @@ const FriendsPage: React.FC = () => {
       setUsers([...users]);
       setRefresh(!refresh);
       // 친구수락 받는 사람한테 알림
-      pushData(userId, userPk)
+      pushData(userId, userPk);
     } catch (error) {}
   };
- 
+
   const handleAcceptFriend = async (friendId: number) => {
     try {
       const response = await axios.post(
@@ -150,7 +159,7 @@ const FriendsPage: React.FC = () => {
       setRefresh(!refresh);
 
       // 친구요청 받는 사람한테 알림
-      pushData(friendId, userPk)
+      pushData(friendId, userPk);
     } catch (error) {
       console.log(error);
     }
@@ -187,131 +196,210 @@ const FriendsPage: React.FC = () => {
     } catch (error) {}
   };
 
+  const PageButtons = ({ totalPages }: { totalPages: number }) => {
+    let buttons = [];
+    if (nowLastNum > totalPages) {
+      setNowLastNum(totalPages);
+    }
+    for (let i = nowStartNum; i <= nowLastNum; i++) {
+      buttons.push(
+        <li className={`page-item ${nowPage == i && 'isNowPage'}`} key={i}>
+          <button
+            className="page-link"
+            onClick={() => {
+              // getData(i),
+              setNowPage(i);
+            }}
+          >
+            {i}
+          </button>
+        </li>,
+      );
+    }
+    return <>{buttons}</>;
+  };
+
+  // Pagination
+  const [totalPages, setTotalPages] = useState(1);
+  const [page, setPage] = useState(1);
+  const [nowPage, setNowPage] = useState<number>(1);
+  const pamount = 5;
+  const [nowStartNum, setNowStartNum] = useState<number>(1);
+  const [nowLastNum, setNowLastNum] = useState<number>(pamount);
+
+  const GoToNextPage = () => {
+    let target = nowLastNum + 1;
+    // if(target)
+    setNowPage(target);
+    // getData(target);
+    setNowStartNum(target);
+    setNowLastNum(nowLastNum + pamount);
+  };
+  const GoToBeforePage = () => {
+    let target = nowStartNum - pamount;
+    if (target < 1) {
+      return;
+    }
+    setNowPage(target);
+    // getData(target);
+    setNowLastNum(nowStartNum - 1);
+    setNowStartNum(target);
+  };
+  const LEFT_IMG =
+    'https://tifyimage.s3.ap-northeast-2.amazonaws.com/c24b625a-30cd-4b39-ad0a-0d7ffddf64c3.png';
+  const RIGHT_IMG =
+    'https://tifyimage.s3.ap-northeast-2.amazonaws.com/a213c7ad-2bc9-489c-87f1-3be671f007f4.png';
+
   return (
     <div className="backcolor">
       <div className="items-center justify-center">
         <div className="find-friends">
           <TapNameEng
             title="Find Friends"
-            content="닉네임을 검색하여 당신의 친구를 찾아보세요."
+            content="친구들을 찾아볼 수 있어요"
           ></TapNameEng>
-          <div className="friends-nickname-box">
-            <input
-              className="friends-nickname-input"
-              type="text"
-              placeholder="닉네임을 입력해 주세요"
-              value={nickname}
-              onKeyUp={(e) => {
-                if (e.key == 'Enter') {
-                  handleSubmit();
-                }
-              }}
-              onChange={(e) => setNickname(e.target.value)}
-            />
-          </div>
         </div>
         <div className="friends-wrap-con">
-          {searchPerformed && !users.length && (
-            <p className="text-center text-xl text-2">
-              일치하는 사용자가 없습니다 다시 검색해주세요
-            </p>
-          )}
-          {searchPerformed && users.length > 0 && (
-            <b>
-              <p className="userCount">일치하는 사용자: {totalSearched}명</p>
-            </b>
-          )}
-          {users.length > 0 && (
-            <div className="friend-list">
-              {users
-                .slice((currentPage - 1) * 8, currentPage * 8)
-                .map((user) => (
-                  <div
-                    className="w-1/5 p-6 mx-3 my-2 items-center justify-center flex flex-col cardcolor cardsize"
-                    key={user.id}
-                  >
-                    <img className="img-4" src={user.profileImg} />
-                    <b>
-                      <p className="w-96 text-xl text-center text-1">
-                        {user.name}
+          <div className="friends-wrap-inner-con">
+            <div className="friends-nickname-box">
+              <input
+                className="friends-nickname-input"
+                type="text"
+                placeholder="닉네임을 입력해 주세요"
+                value={nickname}
+                onKeyUp={(e) => {
+                  if (e.key == 'Enter') {
+                    handleSubmit();
+                  }
+                }}
+                onChange={(e) => setNickname(e.target.value)}
+              />
+              <div
+                className="friends-search-btn"
+                onClick={() => handleSubmit()}
+              >
+                <img src={search_h} className="friends-search-img" />
+              </div>
+            </div>
+            {!searchPerformed && (
+              <p className="userCount">
+                <img src={find_friends} />
+                &nbsp;&nbsp; 친구를 검색해보세요 :)
+              </p>
+            )}
+            {searchPerformed && !users.length && (
+              <p className="userCount">
+                일치하는 사용자가 없습니다 다시 검색해주세요
+              </p>
+            )}
+            {searchPerformed && users.length > 0 && (
+              <p className="userCount">{totalSearched}명이 검색되었습니다.</p>
+            )}
+            {users.length > 0 && (
+              <div className="friend-list">
+                {users
+                  .slice((currentPage - 1) * 8, currentPage * 8)
+                  .map((user) => (
+                    <div
+                      className="w-1/5 p-6 mx-3 my-2 items-center justify-center flex flex-col cardcolor cardsize"
+                      key={user.id}
+                    >
+                      <img className="friend-profImg" src={user.profileImg} />
+                      <b>
+                        <p className="w-96 text-xl text-center text-1">
+                          {user.name}
+                        </p>
+                      </b>
+                      <p className=" w-96 text-center text-2">
+                        {user.nickname}
                       </p>
-                    </b>
-                    <p className="w-96 text-center text-2">{user.nickname}</p>
-                    <p className="w-96 text-xs text-center text 2">
-                      {user.id === userPk ? (
-                        <p>본인</p>
-                      ) : user.state === 'ACCEPTED' ? (
-                        <button
-                          className="rectangle-2-5"
-                          onClick={() => {
-                            handleDeleteFriend(user.friendshipId);
-                          }}
-                        >
-                          친구삭제
-                        </button>
-                      ) : user.state === 'RECEIVED' ? (
-                        <div>
+                      <p className="w-96 text-xs text-center text 2">
+                        {user.id === userPk ? (
+                          <p>me</p>
+                        ) : user.state === 'ACCEPTED' ? (
                           <button
-                            className="rectangle-2-7"
+                            className="friends-req-delete friends-req-delete"
                             onClick={() => {
-                              handleAcceptFriend(user.friendshipId);
+                              handleDeleteFriend(user.friendshipId);
                             }}
                           >
-                            친구수락
+                            친구삭제
                           </button>
+                        ) : user.state === 'RECEIVED' ? (
+                          <div>
+                            <button
+                              className="friends-req-ok"
+                              onClick={() => {
+                                handleAcceptFriend(user.friendshipId);
+                              }}
+                            >
+                              친구수락
+                            </button>
+                            <button
+                              className="friends-req-no"
+                              onClick={() =>
+                                handleCancelFriend(user.friendshipId)
+                              }
+                            >
+                              친구거절
+                            </button>
+                          </div>
+                        ) : user.state === 'REQUESTED' ? (
                           <button
-                            className="rectangle-2-7"
-                            onClick={() =>
-                              handleCancelFriend(user.friendshipId)
-                            }
+                            className="friends-req-no"
+                            onClick={() => {
+                              handleCancelFriend(user.friendshipId);
+                            }}
                           >
-                            친구거절
+                            요청취소
                           </button>
-                        </div>
-                      ) : user.state === 'REQUESTED' ? (
-                        <button
-                          className="rectangle-2-7"
-                          onClick={() => {
-                            handleCancelFriend(user.friendshipId);
-                          }}
-                        >
-                          요청취소
-                        </button>
-                      ) : user.state === 'NONE' ? (
-                        <button
-                          className="rectangle-2-6"
-                          onClick={() => {
-                            handleRequestFriend(user.id);
-                          }}
-                        >
-                          친구요청
-                        </button>
-                      ) : null}
-                    </p>
-                  </div>
-                ))}
-            </div>
-          )}
+                        ) : user.state === 'NONE' ? (
+                          <button
+                            className="friends-req"
+                            onClick={() => {
+                              handleRequestFriend(user.id);
+                            }}
+                          >
+                            친구요청
+                          </button>
+                        ) : null}
+                      </p>
+                    </div>
+                  ))}
+              </div>
+            )}
+          </div>
           <div>
             {users.length > 0 && (
-              <div className="pagination-container-friends">
-                <button
-                  className="pagination-btn bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded-l"
-                  onClick={handlePrevPage}
-                  disabled={currentPage === 1}
-                >
-                  Prev
-                </button>
-                <span className="pagination-text bg-gray-500 font-bold py-2 px-3 rounded-full">
-                  {currentPage} / {Math.ceil(users.length / 8)}
-                </span>
-                <button
-                  className="pagination-btn bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded-l"
-                  onClick={handleNextPage}
-                  disabled={currentPage === Math.ceil(users.length / 8)}
-                >
-                  Next
-                </button>
+              // <div className="friends-paging pagination-container-friends-item">
+              //   <button
+              //     className="pagination-btn bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded-l"
+              //     onClick={handlePrevPage}
+              //     disabled={currentPage === 1}
+              //   >
+              //     Prev
+              //   </button>
+              //   <span className="pagination-text bg-gray-500 font-bold py-2 px-3 rounded-full">
+              //     {currentPage} / {Math.ceil(users.length / 8)}
+              //   </span>
+              //   <button
+              //     className="pagination-btn bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded-l"
+              //     onClick={handleNextPage}
+              //     disabled={currentPage === Math.ceil(users.length / 8)}
+              //   >
+              //     Next
+              //   </button>
+              // </div>
+              <div className="friends-paging-wrap">
+                <ul className="page-btns ">
+                  <div onClick={() => GoToBeforePage()}>
+                    <img src={LEFT_IMG} className="leftPage" />
+                  </div>
+                  <PageButtons totalPages={totalPages} />
+                  <button onClick={() => GoToNextPage()}>
+                    <img src={RIGHT_IMG} className="rightPage" />
+                  </button>
+                </ul>
               </div>
             )}
           </div>
